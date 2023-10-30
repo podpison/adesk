292ff25eb0277e2a8f7bf537effae4a352880d5b
podpison <au25557@yandex.ru>
2023-10-30 14:20:08 +0500

    refactor: list.js

--- blocks/list/index.js
+++ blocks/list/index.js
@@ -7,26 +7,29 @@ const itemIds = [
   'offersAndCooperation'
 ];
 
-const render = () => {
-  console.log('a')
+itemIds.forEach((id) => {
+  const element = document.getElementById(id);
+
+  if (!element) {
+    return;
+  };
+
+  element.addEventListener('click', () => {
+    element.classList.toggle('list_opened');
+  });
+})
+
+window.addEventListener('resize', () => {
   itemIds.forEach((id) => {
     const element = document.getElementById(id);
 
     if (!element) {
       return;
     };
-
     if (window.innerWidth > 600) {
       element.classList.add('list_opened');
     } else {
-      element.addEventListener('click', () => {
-        console.log('b')
-        element.classList.toggle('list_opened');
-      });
+      element.classList.remove('list_opened');
     }
   })
-};
-
-render();
-
-window.addEventListener('resize', render);
+});
