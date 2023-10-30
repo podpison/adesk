7d59f5be0e0faa59ab2c409c9531fd55185bed03
podpison <au25557@yandex.ru>
2023-10-30 14:48:15 +0500

    fix: burger bg

--- blocks/header/index.js
+++ blocks/header/index.js
@@ -3,15 +3,19 @@ const reserved = {
   burgerId: 'burger',
   headerId: 'header',
   closeBurgerId: 'close-burger',
+  headerBg: 'header-bg',
 };
 
 const burgerBtn = document.getElementById(reserved.burgerId);
 const header = document.getElementById(reserved.headerId);
 const closeBurgerMenu = document.getElementById(reserved.closeBurgerId);
+const headerBg = document.getElementById(reserved.headerBg);
 
-burgerBtn.addEventListener('click', () => {
-  header.classList.toggle(reserved.burgerCN);
-});
+[burgerBtn, headerBg].forEach((element) => {
+  element.addEventListener('click', () => {
+    header.classList.toggle(reserved.burgerCN);
+  });
+})
 
 closeBurgerMenu.addEventListener('click', () => {
   header.classList.toggle(reserved.burgerCN);
